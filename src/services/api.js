4aab11f7af4856fbed2068f8587186206ff5ae90
Oba-diah <kipobash@gmail.com>
2026-05-14 02:51:52 +0300

    vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
 })
-
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
